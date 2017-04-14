65d15a508110bd54bf2a51e268db51174d4e2610
Maythee Anegboonlap <null@llun.me>
2017-04-14 20:22:44 +0800

    go fmt

--- bridge.go
+++ bridge.go
@@ -1,64 +1,67 @@
 package hkbridge
 
 import (
-  "log"
-  "net"
+	"log"
+	"net"
 
-  "github.com/brutella/hc"
-  "github.com/brutella/hc/accessory"
-  hclog "github.com/brutella/hc/log"
+	"github.com/brutella/hc"
+	"github.com/brutella/hc/accessory"
+	hclog "github.com/brutella/hc/log"
 )
 
 type Bridge struct {
-  *accessory.Accessory
+	*accessory.Accessory
 }
 
 func NewBridge(config Config) *Bridge {
-  info := accessory.Info{
-    Name:         config.Name,
-    Manufacturer: config.Manufacturer,
-    SerialNumber: config.SerialNumber,
-    Model:        config.Model,
-  }
-  acc := Bridge{
-    Accessory: accessory.New(info, accessory.TypeBridge),
-  }
-  return &acc
+	info := accessory.Info{
+		Name:         config.Name,
+		Manufacturer: config.Manufacturer,
+		SerialNumber: config.SerialNumber,
+		Model:        config.Model,
+	}
+	acc := Bridge{
+		Accessory: accessory.New(info, accessory.TypeBridge),
+	}
+	return &acc
 }
 
 func Start() {
-  config, err := ReadConfig("config.json")
-  if err != nil {
-    log.Fatal(err)
-  }
+	config, err := ReadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  var iface *net.Interface = nil
-  if config.Interface != "" {
-    interfaces, _ := net.Interfaces()
-    for _, targetInterface := range interfaces {
-      if targetInterface.Name == config.Interface {
-        iface = &targetInterface
-      }
-    }
-  }
+	var iface *net.Interface = nil
+	if config.Interface != "" {
 
-  if config.Debug {
-    hclog.Debug.Enable()
-  }
+		interfaces, _ := net.Interfaces()
+		for _, targetInterface := range interfaces {
+			if targetInterface.Name == config.Interface {
+				iface = &targetInterface
+				log.Printf("Setting up accessories on %v", iface.Name)
+				break
+			}
+		}
+	}
 
-  accessories := SetupAccessories(config, iface)
-  bridge := NewBridge(config)
-  t, err := hc.NewIPTransport(hc.Config{
-    Pin:  config.Pin,
-    Port: config.Port,
-  }, bridge.Accessory, accessories...)
-  if err != nil {
-    log.Fatal(err)
-  }
+	if config.Debug {
+		hclog.Debug.Enable()
+	}
 
-  hc.OnTermination(func() {
-    t.Stop()
-  })
+	accessories := SetupAccessories(config, iface)
+	bridge := NewBridge(config)
+	t, err := hc.NewIPTransport(hc.Config{
+		Pin:  config.Pin,
+		Port: config.Port,
+	}, bridge.Accessory, accessories...)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  t.Start()
+	hc.OnTermination(func() {
+		t.Stop()
+	})
+
+	t.Start()
 }
--- config.go
+++ config.go
@@ -1,34 +1,34 @@
 package hkbridge
 
 import (
-  "encoding/json"
-  "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type AccessoryConfig struct {
-  Type   string                 `json:"type"`
-  Option map[string]interface{} `json:"option"`
+	Type   string                 `json:"type"`
+	Option map[string]interface{} `json:"option"`
 }
 
 type Config struct {
-  Name         string            `json:"name"`
-  Manufacturer string            `json:"manufacturer"`
-  SerialNumber string            `json:"serial"`
-  Model        string            `json:"model"`
-  Pin          string            `json:"pin"`
-  Port         string            `json:"port"`
-  Interface    string            `json:"interface"`
-  Debug        bool              `json:"debug"`
-  Accessories  []AccessoryConfig `json:"accessories"`
+	Name         string            `json:"name"`
+	Manufacturer string            `json:"manufacturer"`
+	SerialNumber string            `json:"serial"`
+	Model        string            `json:"model"`
+	Pin          string            `json:"pin"`
+	Port         string            `json:"port"`
+	Interface    string            `json:"interface"`
+	Debug        bool              `json:"debug"`
+	Accessories  []AccessoryConfig `json:"accessories"`
 }
 
 func ReadConfig(file string) (Config, error) {
-  data, err := ioutil.ReadFile(file)
-  if err != nil {
-    return Config{}, err
-  }
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return Config{}, err
+	}
 
-  config := Config{}
-  err = json.Unmarshal(data, &config)
-  return config, err
+	config := Config{}
+	err = json.Unmarshal(data, &config)
+	return config, err
 }
